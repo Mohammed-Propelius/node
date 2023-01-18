c14259e64420b39f3f8059416cd610faf1485f30
Mohammed-Propelius <mohammed.kharodawala@proplius.tech>
2023-01-18 11:37:59 +0530

    event emitter

--- event.js
+++ event.js
@@ -1,6 +1,12 @@
 // Event emitter
 const EventEmitter = require('events')
-const myEmitter = new EventEmitter()
+const http = require('http')
+class Sales extends EventEmitter{
+    constructor() {
+        super();
+    }
+}
+const myEmitter = new Sales()
 myEmitter.on("newSale",() => {
     console.log("There was a sales!!")
 })
@@ -12,3 +18,21 @@ myEmitter.on("newSale",(stocks) => {
     console.log(`There were ${stocks} items left in stocks`)
 })
 myEmitter.emit("newSale",9)
+// ----------------------------------------------------------
+const server = http.createServer()
+server.on('request',(req,res)=>{
+    console.log("Req received")
+    res.end('Request received')
+})
+
+server.on('request',(req,res)=>{
+   console.log("another request")
+})
+
+server.on('close',() => {
+    console.log('Server Close')
+})
+
+server.listen(8000,'127.0.0.1',()=>{EventEmitter
+    console.log("Waiting for req")
+})
